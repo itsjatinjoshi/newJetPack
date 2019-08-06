f4a72ff17fdee9c461911621bbdae8781ec068fe
1893888 <jatinjoshi2283@gmail.com>
2019-08-06 16:39:54 -0400

    Initial commit

---
+++ app/src/main/java/com/example/newjetpack/defaultFragment.java
@@ -0,0 +1,31 @@
+package com.example.newjetpack;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.zip.Inflater;
+
+
+public class defaultFragment extends Fragment {
+    @Override
+    public void onAttach(@NonNull Context context) {
+
+
+        super.onAttach(context);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_default, container, false);
+    }
+}
